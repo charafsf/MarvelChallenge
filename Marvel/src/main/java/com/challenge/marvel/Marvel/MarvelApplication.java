d7336fa9fd7ad1bee1251fd2d2a7be7d85e71ec1
charafSfaoua <sfaouacharaf2@gmail.com>
2020-12-07 13:12:35 +0100

    add sources folder  / initialisation project

---
+++ Marvel/src/main/java/com/challenge/marvel/Marvel/MarvelApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.marvel.Marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApplication.class, args);
+	}
+
+}
